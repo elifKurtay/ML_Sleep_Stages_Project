9e0a16d5b904d78bc0788d21c289846a37632904
elifkurtay <elif.kurtay00@gmail.com>
2022-11-18 16:56:57 +0100

    clear run

--- scripts/run.py
+++ scripts/run.py
@@ -4,11 +4,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     print('This will be run.py')
-    path = get_write_path()
-    for subjectID in PARTICIPANT_IDS:
-        data = get_sleepstages(subjectID)
-        if data is not None:
-            data.to_csv(path + "/SMS_" + subjectID + ".csv", index=True)
+    data = get_sleepstages(PARTICIPANT_IDS[0])
 
     pd.set_option('display.max_columns', None)
     print(data.head())
