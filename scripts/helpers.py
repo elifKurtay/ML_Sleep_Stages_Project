3488754b133e232576119253fca85f4dcee3b68e
elifkurtay <elif.kurtay00@gmail.com>
2022-11-22 11:04:46 +0100

    ml model added

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -27,6 +27,28 @@ def test_imputing():
     return votes
 
 
+def impute_data(subjectID):
+    """ impute all columns of patient data
+    1. impute using interpolate for middle values
+    2. for the beginning and ending values perform both bfill and ffill
+
+    return type: array of size 2
+    array[0] = new dataframe with imputed values
+    array[1] = current nan values of each column in order
+    """
+    sleep_stages = read_patient_data(subjectID)
+    sleep_stages["sleep_stage_num_somnofy"] = sleep_stages["sleep_stage_num_somnofy"]\
+        .interpolate(option='time').round().bfill().ffill()
+    sleep_stages["sleep_stage_num_emfit"] = sleep_stages["sleep_stage_num_emfit"]\
+        .interpolate(option='time').round().bfill().ffill()
+    sleep_stages["sleep_stage_num_psg"] = sleep_stages["sleep_stage_num_psg"] \
+        .interpolate(option='time').round().bfill().ffill()
+    nan_count = sleep_stages.isna().sum()
+    if nan_count[2] == sleep_stages.shape[0]:
+        print("NO PSG FOR PATIENT " + subjectID)
+    return sleep_stages, (nan_count[0], nan_count[1], nan_count[2])
+
+
 def impute_all():
     """ impute all columns of all patient data
     1. impute using interpolate for middle values
@@ -38,15 +60,6 @@ def impute_all():
     """
     dfs = []
     for subjectID in PARTICIPANT_IDS:
-        sleep_stages = read_patient_data(subjectID)
-        sleep_stages["sleep_stage_num_somnofy"] = sleep_stages["sleep_stage_num_somnofy"]\
-            .interpolate(option='time').round().bfill().ffill()
-        sleep_stages["sleep_stage_num_emfit"] = sleep_stages["sleep_stage_num_emfit"]\
-            .interpolate(option='time').round().bfill().ffill()
-        sleep_stages["sleep_stage_num_psg"] = sleep_stages["sleep_stage_num_psg"] \
-            .interpolate(option='time').round().bfill().ffill()
-        nan_count = sleep_stages.isna().sum()
+        sleep_stages, nan_count = impute_data(subjectID)
         dfs.append([sleep_stages, (nan_count[0], nan_count[1], nan_count[2])])
-        if nan_count[2] == sleep_stages.shape[0]:
-            print("NO PSG FOR PATIENT " + subjectID)
     return dfs
