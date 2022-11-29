d8f2e29fedb2e2204773c6f5289ff4dff5128a30
elifkurtay <elif.kurtay00@gmail.com>
2022-11-29 14:27:17 +0100

    naive bayes

--- scripts/plot.py
+++ scripts/plot.py
@@ -24,7 +24,7 @@ def plot_missing_sleepstages(df):
 
 
 def plot_conf_matrix(preds, labels):
-    conf_matrix = confusion_matrix(preds, labels)
+    conf_matrix = confusion_matrix(labels, preds)
     print(conf_matrix)
     df_cm = pd.DataFrame(conf_matrix, range(4), range(4))
     ax = sn.heatmap(df_cm, annot=True, annot_kws={"size": 14}, fmt=".3g")
