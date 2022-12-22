d4562a4fec7ba4e50fd3efeee5fa1de7c46cbbed
elifkurtay <elif.kurtay00@gmail.com>
2022-12-22 03:27:15 +0100

    update

--- scripts/run.py
+++ scripts/run.py
@@ -1,25 +1,27 @@
+import keras as keras
+from keras.utils import to_categorical
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import CategoricalNB
 
 from loader import *
 
 if __name__ == '__main__':
-    print("The best model for our data is scikit-learn's CategoricalNB classifier.")
-    # load processed data
+    print("The best model for our data is our sequential CNN model.")
+    # load processed data and model
     x, y, _, _ = get_nn_patients()
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, shuffle=False, stratify=None)
-    train_size, test_size = x_train.shape[0] * x_train.shape[1], x_test.shape[0] * x_test.shape[1]
-    x_tr, x_te, y_tr, y_te = x_train.reshape(train_size, 2), x_test.reshape(test_size, 2), y_train\
-        .ravel(), y_test.ravel()
+    x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size=0.30, shuffle=False, stratify=None)
+    train_size, test_size = x_tr.shape[0] * x_tr.shape[1], x_te.shape[0] * x_te.shape[1]
+    trainX = x_tr.reshape(train_size, 1, 2)
+    trainy = y_tr.reshape(train_size)
+    testX = x_te.reshape(test_size, 1, 2)
+    y_te = y_te.reshape(test_size)
 
-    # train CategoricalNB classifier
-    cnb_classifier = CategoricalNB()
-    cnb_classifier.fit(x_tr, y_tr)
-    preds = cnb_classifier.predict(x_te)
+    trainy = to_categorical(trainy)
+    testy = to_categorical(y_te)
+    model = keras.models.load_model('path/to/location')
 
-    # observe results
-    print(classification_report(y_te, preds))
-    print("Balanced accuracy:", balanced_accuracy_score(y_te, preds))
-    print("Accuracy:", accuracy_score(y_te, preds))
-    plot_conf_matrix(preds, y_te, normalize=True)
+
+    #print(classification_report(true, preds))
+    #print("Balanced accuracy:", balanced_accuracy_score(true, preds))
+    #print("Accuracy:", accuracy_score(true, preds))
+    #plot_conf_matrix(true, y_te, normalize=True)
